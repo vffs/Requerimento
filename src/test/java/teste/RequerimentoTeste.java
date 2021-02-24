70028a65c87100c1cd177d4e2154084acb73d3e4
vffs <vffs@a.recife.ifpe.edu.br>
2021-02-24 15:17:16 -0300

    Criacao das classes de testes Teste e RequerimentoTeste

---
+++ src/test/java/teste/RequerimentoTeste.java
@@ -0,0 +1,52 @@
+
+package teste;
+
+import java.util.Date;
+import javax.naming.NamingException;
+import model.Requerimento;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import service.RequerimentoService;
+
+/**
+ *
+ * @author valeria
+ */
+public class RequerimentoTeste extends Teste{
+    RequerimentoService requerimentoService;
+    Requerimento requerimento;
+    Date dataInclusao;
+    
+    @Before
+    public void setup() throws NamingException{
+        requerimentoService = (RequerimentoService) container.getContext().lookup("java:global/classes/RequerimentoService!service.RequerimentoService");
+    }
+    
+    @After
+    public void teardown(){
+        requerimentoService = null;
+        requerimento = null;
+    }
+    
+    @Test
+    public void t01_persistirRequerimento(){
+        dataInclusao = new Date();
+        requerimento = requerimentoService.criar();
+        requerimento.setNome("Letícia katarina Silva");
+        requerimento.setMatricula("XYZ444");
+        requerimento.setEmail("leticia@gmail.com");
+        requerimento.setTurno("noite");
+        requerimento.setCurso("Administração");
+        requerimento.setPeriodoSerie("Concluiu");
+        requerimento.setProblema("Solicitar Diploma");
+        requerimento.setDataInclusao(dataInclusao);
+        requerimento.setObservacoes("Necessito do diploma para ser promovida");
+        requerimentoService.salvar(requerimento);
+        assertNotNull(requerimento.getIdRequerimento());
+    }
+    
+    
+}
---
+++ src/test/java/teste/Teste.java
@@ -0,0 +1,29 @@
+
+package teste;
+
+import java.util.logging.Logger;
+import javax.ejb.embeddable.EJBContainer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author valeria
+ */
+public class Teste {
+    
+    protected static EJBContainer container;
+    protected final Logger logger = Logger.getGlobal();
+    
+    @BeforeClass
+    public static void setUpClass(){
+        container = EJBContainer.createEJBContainer();
+        DbUnitUtil.inserirDados();
+    }
+    
+    @AfterClass
+    public static void tearDownClass(){
+        container.close();
+    }
+    
+}
