fb8320cafaf8c524f8139a03060616c3e34f4899
vffs <vffs@a.recife.ifpe.edu.br>
2021-02-25 10:40:17 -0300

    incluido teste alterar, deletar e listarTodos

--- src/test/java/teste/RequerimentoTeste.java
+++ src/test/java/teste/RequerimentoTeste.java
@@ -2,11 +2,13 @@
 package teste;
 
 import java.util.Date;
+import java.util.List;
 import javax.naming.NamingException;
 import model.Requerimento;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 import service.RequerimentoService;
@@ -49,6 +51,28 @@ public class RequerimentoTeste extends Teste{
         assertNotNull(requerimento.getIdRequerimento());
     }
     
+    @Test
+    public void t02_AlterarRequerimento(){
+        requerimento = requerimentoService.consultarPorId(2L);
+        requerimento.setEmail("anaclair@gmail.com");
+        requerimentoService.alterar(requerimento);
+        requerimento = requerimentoService.consultarPorId(2L);
+        assertEquals("anaclair@gmail.com",requerimento.getEmail());
+    }
+    
+    @Test
+    public void t03_deletarRequerimento(){
+        requerimento = requerimentoService.consultarPorId(3L);
+        requerimentoService.deletar(requerimento);
+        requerimento = requerimentoService.consultarPorId(3L);
+        assertNull(requerimento);
+    }
+    
+    @Test
+    public void t04_listarTodosRequerimentos(){
+        List<Requerimento> requerimentos = requerimentoService.listarTodos();
+        assertEquals(3, requerimentos.size());
+    }
     
     
 }
